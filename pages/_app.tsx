6f6e4048c74862c9ba3299e04ae5f8ce75e1ad81
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2022-04-28 22:46:44 +0200

    App Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import PlayerLayout from '../components/playerLayout';
+import 'reset-css';
 
-export default MyApp
+const theme = extendTheme({
+  color: {
+    gray: {
+      200: '#F5F5F5',
+      100: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121'
+    }
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none'
+          }
+        }
+      }
+    }
+  }
+})
+
+const MyApp = ({ Component, pageProps }) => (
+  <ChakraProvider>
+    <PlayerLayout>
+      <Component {...pageProps} />
+    </PlayerLayout>
+  </ChakraProvider>
+);
+
+export default MyApp;
